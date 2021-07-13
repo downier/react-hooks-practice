58ccc50c482fc9bda4a3eb1445a111f01647b72f
downier <raulishere@gmail.com>
2021-07-13 13:11:46 -0700

    Added react router and auth0

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
 import { Search } from './Search';
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
+
+const Home = () => (
+  <div>
+    <h2> Please login with auth0 to search books </h2>
+  </div>
+);
 
 function App() {
   return (
     <div className="App">
-      <Search/>
+      <header>
+        <ul className="menu">
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/search">Search</Link></li>
+        </ul>
+      </header>
+      <div>
+        <Switch>
+          <Route exact path="/" component={Home}/>
+          <Route path="/search" component={Search}/>
+          <Route render={() => (<div> Sorry, this page does not exist. </div>)} />
+        </Switch>
+      </div>
     </div>
   );
 }
