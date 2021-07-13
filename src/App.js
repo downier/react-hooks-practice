858beb86fe6b4bfd7874103f598161189700af60
downier <raulishere@gmail.com>
2021-07-13 13:20:44 -0700

    Saved work, need auth0 key

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
 import { Search } from './Search';
 import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
+import { withRouter } from 'react-router';
+import Callback from './Callback/Callback';
 
 const Home = () => (
   <div>
@@ -8,8 +10,37 @@ const Home = () => (
   </div>
 );
 
-function App() {
+function HomePage(props) {
+  const { authenticated } = props;
+
+  const logout = () => {
+    props.auth.logout();
+    props.history.push('/');
+  };
+
+  if (authenticated) {
+    const { name } = props.auth.getProfile();
+    return (
+      <div>
+        <h1>Howdy! Glad to see you back, {name}.</h1>
+        <button onClick={logout}>Log out</button>
+      </div>
+    );
+  }
+
   return (
+    <div>
+      <h1>I don't know you. Please, log in.</h1>
+      <button onClick={props.auth.login}>Log in</button>
+    </div>
+  );
+}
+
+function App(props) {
+  const authenticated = props.auth.isAuthenticated();
+
+  return (
+    
     <div className="App">
       <header>
         <ul className="menu">
